116298543d113014acdcb443fe65cc7770e4a130
csteele <csteele@mastercontrol.com>
2021-01-29 14:08:26 -0700

    Uploading repo

---
+++ frontend/src/app/challenge-solved-notification/challenge-solved-notification.component.spec.ts
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2014-2021 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { ClipboardModule } from 'ngx-clipboard'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { CountryMappingService } from '../Services/country-mapping.service'
+import { CookieService } from 'ngx-cookie-service'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
+import { ChallengeService } from '../Services/challenge.service'
+import { ConfigurationService } from '../Services/configuration.service'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing'
+import { SocketIoService } from '../Services/socket-io.service'
+
+import { ChallengeSolvedNotificationComponent } from './challenge-solved-notification.component'
+import { of } from 'rxjs'
+import { EventEmitter } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
+
+class MockSocket {
+  on (str: string, callback: Function) {
+    callback()
+  }
+}
+
+describe('ChallengeSolvedNotificationComponent', () => {
+  let component: ChallengeSolvedNotificationComponent
+  let fixture: ComponentFixture<ChallengeSolvedNotificationComponent>
+  let socketIoService: any
+  let translateService: any
+  let cookieService: any
+  let challengeService: any
+  let mockSocket: any
+
+  beforeEach(waitForAsync(() => {
+    mockSocket = new MockSocket()
+    socketIoService = jasmine.createSpyObj('SocketIoService', ['socket'])
+    socketIoService.socket.and.returnValue(mockSocket)
+    translateService = jasmine.createSpyObj('TranslateService', ['get'])
+    translateService.get.and.returnValue(of({}))
+    translateService.onLangChange = new EventEmitter()
+    translateService.onTranslationChange = new EventEmitter()
+    translateService.onDefaultLangChange = new EventEmitter()
+    cookieService = jasmine.createSpyObj('CookieService', ['set'])
+    challengeService = jasmine.createSpyObj('ChallengeService', ['continueCode'])
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        ClipboardModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule
+      ],
+      declarations: [ChallengeSolvedNotificationComponent],
+      providers: [
+        { provide: SocketIoService, useValue: socketIoService },
+        { provide: TranslateService, useValue: translateService },
+        { provide: CookieService, useValue: cookieService },
+        { provide: ChallengeService, useValue: challengeService },
+        ConfigurationService,
+        CountryMappingService
+      ]
+    })
+      .compileComponents()
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChallengeSolvedNotificationComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should delete notifictions', () => {
+    component.notifications = [
+      { message: 'foo', flag: '1234', copied: false },
+      { message: 'bar', flag: '5678', copied: false }
+    ]
+    component.closeNotification(0)
+
+    expect(component.notifications).toEqual([{ message: 'bar', flag: '5678', copied: false }])
+  })
+
+  it('should delete all notifications if the shiftKey was pressed', () => {
+    component.notifications = [
+      { message: 'foo', flag: '1234', copied: false },
+      { message: 'bar', flag: '5678', copied: false }
+    ]
+    component.closeNotification(0, true)
+
+    expect(component.notifications).toEqual([])
+  })
+
+  it('should add new notification', fakeAsync(() => {
+    translateService.get.and.returnValue(of('CHALLENGE_SOLVED'))
+    component.notifications = []
+    component.showNotification({ challenge: 'Test', flag: '1234' })
+    tick()
+
+    expect(translateService.get).toHaveBeenCalledWith('CHALLENGE_SOLVED', { challenge: 'Test' })
+    expect(component.notifications).toEqual([{ message: 'CHALLENGE_SOLVED', flag: '1234', copied: false, country: undefined }])
+  }))
+
+  it('should throw error when not supplied with a valid continue code', () => {
+    challengeService.continueCode.and.returnValue(of(undefined))
+    console.log = jasmine.createSpy('log')
+
+    expect(component.saveProgress).toThrow()
+  })
+})
---
+++ frontend/src/app/sidenav/sidenav.component.spec.ts
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2014-2021 Bjoern Kimminich.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { ChallengeService } from '../Services/challenge.service'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { SocketIoService } from '../Services/socket-io.service'
+import { ConfigurationService } from '../Services/configuration.service'
+import { TranslateModule, TranslateService } from '@ngx-translate/core'
+import { RouterTestingModule } from '@angular/router/testing'
+import { of } from 'rxjs'
+import { HttpClientModule } from '@angular/common/http'
+import { CookieService } from 'ngx-cookie-service'
+import { LoginGuard } from '../app.guard'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { SidenavComponent } from './sidenav.component'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatListModule } from '@angular/material/list'
+import { roles } from '../roles'
+import { AdministrationService } from '../Services/administration.service'
+
+class MockSocket {
+  on (str: string, callback: Function) {
+    callback(str)
+  }
+}
+
+describe('SidenavComponent', () => {
+  let component: SidenavComponent
+  let fixture: ComponentFixture<SidenavComponent>
+  let challengeService: any
+  let cookieService: any
+  let configurationService: any
+  let administractionService: any
+  let mockSocket: any
+  let socketIoService: any
+  let loginGuard
+
+  beforeEach(waitForAsync(() => {
+    configurationService = jasmine.createSpyObj('ConfigurationService', ['getApplicationConfiguration'])
+    configurationService.getApplicationConfiguration.and.returnValue(of({ application: { welcomeBanner: {} }, hackingInstructor: {} }))
+    challengeService = jasmine.createSpyObj('ChallengeService', ['find'])
+    challengeService.find.and.returnValue(of([{ solved: false }]))
+    administractionService = jasmine.createSpyObj('AdministrationService', ['getApplicationVersion'])
+    administractionService.getApplicationVersion.and.returnValue(of(null))
+    cookieService = jasmine.createSpyObj('CookieService', ['delete', 'get', 'set'])
+    mockSocket = new MockSocket()
+    socketIoService = jasmine.createSpyObj('SocketIoService', ['socket'])
+    socketIoService.socket.and.returnValue(mockSocket)
+    loginGuard = jasmine.createSpyObj('LoginGuard', ['tokenDecode'])
+    loginGuard.tokenDecode.and.returnValue({})
+
+    TestBed.configureTestingModule({
+      declarations: [SidenavComponent],
+      imports: [
+        HttpClientModule,
+        TranslateModule.forRoot(),
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatListModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: ConfigurationService, useValue: configurationService },
+        { provide: ChallengeService, useValue: challengeService },
+        { provide: AdministrationService, useValue: administractionService },
+        { provide: CookieService, useValue: cookieService },
+        { provide: SocketIoService, useValue: socketIoService },
+        { provide: LoginGuard, useValue: loginGuard },
+        TranslateService
+      ]
+    })
+      .compileComponents()
+    TestBed.inject(TranslateService)
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SidenavComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should show accounting functionality when user has according role', () => {
+    loginGuard.tokenDecode.and.returnValue({ data: { role: roles.accounting } })
+
+    expect(component.isAccounting()).toBe(true)
+  })
+
+  it('should set version number as retrieved with "v" prefix', () => {
+    loginGuard.tokenDecode.and.returnValue({ data: { role: roles.accounting } })
+
+    expect(component.isAccounting()).toBe(true)
+  })
+
+  it('should not show accounting functionality when user lacks according role', () => {
+    administractionService.getApplicationVersion.and.returnValue(of('1.2.3'))
+    component.ngOnInit()
+
+    expect(component.version).toBe('v1.2.3')
+  })
+
+  it('should hide Score Board link when Score Board was not discovered yet', () => {
+    challengeService.find.and.returnValue(of([{ name: 'Score Board', solved: false }]))
+    component.getScoreBoardStatus()
+
+    expect(component.scoreBoardVisible).toBe(false)
+  })
+
+  it('should show Score Board link when Score Board was already discovered', () => {
+    challengeService.find.and.returnValue(of([{ name: 'Score Board', solved: true }]))
+    component.getScoreBoardStatus()
+
+    expect(component.scoreBoardVisible).toBe(true)
+  })
+})
